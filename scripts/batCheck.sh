a70949f5dfd4ad28bdb5b93083dc4d1f49ab1826
Oscar Flores <oscar.floresl@udem.edu>
2025-01-10 22:34:50 -0600

    updated dunst calls to include icons

--- scripts/batCheck.sh
+++ scripts/batCheck.sh
@@ -25,13 +25,13 @@ if [ $BATTERIES != '0' ]; then
         if [ "$CHARGING" = "FALSE" ]; then
             if [ $AVERAGE_CAPACITY -le $CRITICAL ]; then
                 if [ $LAST != 'CRIT' ]; then
-                    notify-send "BATTERY CRITICAL" "Battery at ${AVERAGE_CAPACITY}%"
+                    notify-send -i battery-level-20-symbolic "LOW BATTERY" "Battery at ${AVERAGE_CAPACITY}%"
                     LAST='CRIT'
                 fi
 
             elif [ $AVERAGE_CAPACITY -le $LOW ]; then
                 if [ $LAST != 'LOW' ]; then
-                    notify-send "LOW BATTERY" "Battery at ${AVERAGE_CAPACITY}%"
+                    notify-send -i battery-level-10-symbolic "BATTERY CRITICAL" "Battery at ${AVERAGE_CAPACITY}%"
                     LAST='LOW'
                 fi
             fi
