27e7b28380e9cf8b03cff7ecb6933c2227a6b81a
Oscar Flores <oscar.floresl@udem.edu>
2025-01-31 20:50:12 -0600

    updated markdown renderer configs

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,10 +1,20 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+  -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
-  config = function() vim.cmd [[RenderMarkdown disable]] end,
+  opts = {
+    heading = {
+      enabled = true,
+      position = "overlay",
+    },
+    code = {
+      -- Hide second icon and name
+      sign = false,
+      language_name = false,
+    },
+  },
+  --config = function() vim.cmd [[RenderMarkdown disable]] end,
 }
