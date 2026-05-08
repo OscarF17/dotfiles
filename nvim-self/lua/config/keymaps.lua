2af5115520ea53dce2c817d6184efcaf666d832a
Oscar Flores <oscarlflores@hotmail.com>
2026-05-08 09:41:52 -0600

    separated astronvim config from my own config

---
+++ nvim-self/lua/config/keymaps.lua
@@ -0,0 +1,20 @@
+-- Moving keymaps to the lazy.lua file can improve performance as creating keymaps for plugins forces LazyNvim to load them on startup. If a keymap isn't here, check there.
+
+local keymap = vim.keymap.set
+
+-- Move between windows using Ctrl + hjkl
+keymap("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+keymap("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+keymap("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+keymap("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+-- Neotree
+keymap("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle File Explorer" })
+keymap("n", "<leader>o", ":Neotree focus<CR>", { desc = "Focus File Explorer" })
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
---
+++ nvim-self/lua/config/options.lua
@@ -0,0 +1,32 @@
+-- Display line numbers on the left side of the screen
+vim.opt.number = true
+
+-- Show the line number relative to the line your cursor is on
+vim.opt.relativenumber = true
+
+-- Use spaces instead of actual tab characters when you press Tab
+vim.opt.expandtab = true
+
+-- The number of spaces a Tab character represents
+vim.opt.tabstop = 4
+
+-- The number of spaces used for each level of automatic indentation
+vim.opt.shiftwidth = 4
+
+-- Highlight the search results as you type your search query
+vim.opt.incsearch = true
+
+-- Keep search results highlighted after you press enter
+vim.opt.hlsearch = true
+
+-- Ignore case when searching
+vim.opt.ignorecase = true
+
+-- If the search contains a capital letter, it becomes case-sensitive
+vim.opt.smartcase = true
+
+-- Enable mouse support for clicking and scrolling
+vim.opt.mouse = "a"
+
+-- Wrap long lines of text to the next line visually
+vim.opt.wrap = true 
