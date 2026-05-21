03a626fefdc9118a7c963b7ae572171bc8acc43b
Oscar Flores <oscarlflores@hotmail.com>
2026-05-20 23:13:02 -0600

    reformatted quotes

--- nvim-self/lua/config/keymaps.lua
+++ nvim-self/lua/config/keymaps.lua
@@ -21,11 +21,11 @@ keymap("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle File Explorer" 
 keymap("n", "<leader>o", ":Neotree focus<CR>", { desc = "Focus File Explorer" })
 
 -- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 vim.keymap.set("n", "<leader>th", "<cmd>Telescope colorscheme enable_preview=true<cr>", { desc = "Switch Themes" })
 
 -- Lazygit
